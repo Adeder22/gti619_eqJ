6c151290092964250654058746f402ae55e83b71
Walter <zanderlavastory@gmail.com>
2025-03-24 20:51:20 -0400

    Added users table

--- database/migrations/2025_03_25_004650_create_users_table.php
+++ database/migrations/2025_03_25_004650_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,17 @@ class CreateClientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->unsignedBigInteger('role_id');
+            $table->string('name');
+            $table->string('password');
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
         });
     }
 
@@ -28,6 +34,6 @@ class CreateClientsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('users');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Role;
-use App\Models\Client;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
         Role::insert(['name' => 'Préposé aux clients résidentiels']);
         Role::insert(['name' => 'Préposé aux clients d’affaire']);
 
-        Client::insert(['name' => 'Administrateur', 'password' => 'admin123', 'role_id' => 1]);
-        Client::insert(['name' => 'Utilisateur 1', 'password' => 'user1', 'role_id' => 2]);
-        Client::insert(['name' => 'Utilisateur 2', 'password' => 'user2', 'role_id' => 3]);
+        User::insert(['name' => 'Administrateur', 'password' => 'admin123', 'role_id' => 1]);
+        User::insert(['name' => 'Utilisateur 1', 'password' => 'user1', 'role_id' => 2]);
+        User::insert(['name' => 'Utilisateur 2', 'password' => 'user2', 'role_id' => 3]);
 
         // \App\Models\User::factory(10)->create();
     }
